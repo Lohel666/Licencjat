954c66c901dfaf036b46a612edbb6ace68c99945
Maciej <maciej.boba86@gmail.com>
2018-09-26 09:18:27 +0200

    configuration plus tutorial part 1

--- simple-blobs.py
+++ simple-blobs.py
@@ -3,12 +3,14 @@
 # different classes
 
 #imports
+import numpy as np
 from sklearn.datasets import make_blobs
-import numpy as np 
 import matplotlib.pyplot as plt
+import os
 os.environ["TFF_CPP_MIN_LOG_"]="2"
 
 # helper functions
+
 
 # plot the data on a figure
 def plot_data(pl, X, y):
@@ -19,11 +21,12 @@ def plot_data(pl, X, y):
     pl.legend(['0', '1'])
     return pl
 
+
 # common function that draws the decision boundaries
 def plot_decision_boundary(model, X, y):
 
     amin, bmin = X.min(axis=0) - 0.1
-    amax, bman = X.max(axis=0) + 0.1
+    amax, bmax = X.max(axis=0) + 0.1
     hticks = np.linspace(amin, amax, 101)
     vticks = np.linspace(bmin, bmax, 101)
     
@@ -34,7 +37,7 @@ def plot_decision_boundary(model, X, y):
     c = model.predict(ab)
     Z = c.reshape(aa.shape)
 
-    plt.figure(figsize=(12, 8)
+    plt.figure(figsize=(12, 8))
     # plot the contour
     plt.contourf(aa, bb, Z, cmap='bwr', alpha=0.2)
     # plot the moons of data
@@ -48,4 +51,36 @@ def plot_decision_boundary(model, X, y):
 # y is a [number of samples] sized array. y[sample] contains the class index (ie. 0 or 1 when there are 2 centers)
 # ex: y[1] = 0, y[1] = 1
 
+
 X, y = make_blobs(n_samples=1000, centers=2, random_state=42)
+
+pl = plot_data(plt, X, y)
+pl.show()
+
+#SPlit the data into Training and Test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Create the Keras model
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+# Simple Sequential model
+model = Sequential()
+# Add a Dense Fully Connected Layer with 1 neuron. Using input_shape = (2,) says the input will
+#  be arrays of the form (*,2). The first dimension will be unspecified
+#  number of batches (rows) of data. The second dimension is 2 which are the X, Y positions of each data element.
+#  The sigmoid activation function is used to return 0 or 1, signifying the data
+#  cluster the position is predicted to belong to.
+model.add(Dense(1, input_shape=(2,), activation="sigmoid"))
+# Compile the model. Minimize crossentropy for a binary. Maximize for accuracy
+model.compile(Adam(lr=0.05), 'binary_crossentropy', metrics=['accuracy'])
+# Fit the model with the data from make_blobs. Make 100cycles through the data
+#  Set verbose to 0 to supress progress messages
+model.fit(X_train, y_train, epochs=100, verbose=0)
+# Get loss and accuracy on test data
+eval_result = model.evaluate(X_test, y_test)
+# Print test accuracy
+print("\n\nTest loss: ", eval_result[0], "Test accuracy: ", eval_result[1])
+plot_decision_boundary(model, X, y).show()
